3f79c253da84bb9b2016855ed4e9a3130d6215bc
Chris-Emma <chrisemmaofficial@gmail.com>
2024-05-31 13:02:35 +0000

    schemas

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+"""Instantiating the FastAPI app"""
 
 @app.get('/')
 def read_root():
+    """Function that returns the '/' route"""
     return "Welcome to the URL shortener"
