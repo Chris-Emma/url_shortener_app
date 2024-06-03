294e34b35fd0cd50b2e8cd30b3c85ecbc18e3274
Chris-Emma <chrisemmaofficial@gmail.com>
2024-06-03 07:32:47 +0000

    main

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI
+import validators
+from fastapi import FastAPI, HTTPException
+
+from . import schemas
 
 app = FastAPI()
 """Instantiating the FastAPI app"""
+
+def raise_bad_request(message):
+    """Raises an exception error if url provided isnt valid"""
+    raise HTTPException(status_code=400, detail=message)
 
 @app.get('/')
 def read_root():
     """Function that returns the '/' route"""
     return "Welcome to the URL shortener"
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    """
+    Endpoint to create shortened url
+    
+    """
