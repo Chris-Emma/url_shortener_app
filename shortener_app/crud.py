0493a082d9c64bb5dada45bdb52f37e124a2bf1a
Chris-Emma <chrisemmaofficial@gmail.com>
2024-06-05 11:04:41 +0000

    shortener

---
+++ shortener_app/crud.py
@@ -0,0 +1,61 @@
+from sqlalchemy.orm import Session
+
+from . import keygen, models, schemas
+
+def create_db_url(db: Session, url: schemas.URLBase) -> models:
+    """
+    Create a new URL entry in the database.
+    Parameters:
+    - db (Session): SQLAlchemy database session
+    - url (URLBase): Schema representing the URL to be shortened
+    Returns:
+    - URL: Newly created URL entry in the database
+    """
+    key = keygen.create_unique_random_key(db)
+    secret_key = f"{key}_{keygen.create_random_key(length=8)}"
+    db_url = models.URL(
+        target_url=url.target_url, key=key, secret_key=secret_key
+    )
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    return db_url
+
+def get_db_url_by_key(db: Session, url_key:str) -> models.URL:
+    """
+    Retrieve a URL entry from the database based on the given key.
+
+    Parameters:
+    - db (Session): SQLAlchemy database session
+    - url_key (str): Key associated with the URL entry to be retrieved
+
+    Returns:
+    - URL: URL entry with the given key, or None if not found or inactive
+    """
+    return (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+
+def get_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
+    return (
+        db.query(models.URL)
+        .filter(models.URL.secret_key == secret_key, models.URL.is_active)
+        .first()
+    )
+
+def update_db_clicks(db: Session, db_url: schemas.URL) -> models.URL:
+    db_url.clicks += 1
+    db.commit()
+    db.refresh(db_url)
+    return db_url
+
+def deactivate_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
+    db_url = get_db_url_by_secret_key(db, secret_key)
+    if db_url:
+        db_url.is_active = False
+        db.commit()
+        db.refresh(db_url)
+
+    return db_url
--- shortener_app/keygen.py
+++ shortener_app/keygen.py
@@ -1,8 +1,29 @@
 import secrets
 import string
 
+from sqlalchemy.orm import Session
+from . import crud
+
 def create_random_key(length: int = 5) -> str:
-    """String module to generate ASCII characters
-    - secrets module secretly choses five characters from chars"""
+    """
+    Generate a random key with the specified length using uppercase letters and digits.
+    Parameters:
+    - length (int): Length of the random key (default is 5)
+    Returns:
+    - str: Randomly generated key
+    """
     chars = string.ascii_uppercase + string.digits
     return "".join(secrets.choice(chars) for _ in range(length))
+
+def create_unique_random_key(db: Session) -> str:
+    """
+    Generate a unique random key that is not already in use in the database.
+    Parameters:
+    - db (Session): SQLAlchemy database session
+    Returns:
+    - str: Unique randomly generated key
+    """
+    key = create_random_key()
+    while crud.get_db_url_by_key(db, key):
+        key = create_random_key()
+    return key
