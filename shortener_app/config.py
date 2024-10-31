ead2d9362738f8e4342da18f8e4c52701c310bbd
Chris-Emma <chrisemmaofficial@gmail.com>
2024-10-31 07:17:41 +0000

    documenting config file

--- shortener_app/config.py
+++ shortener_app/config.py
@@ -2,18 +2,31 @@ from functools import lru_cache
 
 from pydantic import BaseSettings
 
-
 class Settings(BaseSettings):
+    """
+    A configuration class that defines settings for the application
+    Attributes:
+        env_name (str): The name of the environment
+        base_url (str): The base URL of the application
+        db_url (str): The database URL.
+    Config:
+        env_file (str): Specifies the path to an .env file to load environment variables.
+    """
     env_name: str = "Local"
     base_url: str = "http://localhost:8000"
     db_url: str = "sqlite:///./shortener.db"
 
-    class Config:
-        env_file = ".env"
-
+class Config:
+    env_file = ".env"
 
 @lru_cache
 def get_settings() -> Settings:
+    """
+    Retrieves the application settings as a singleton,
+    using an in-memory cache to optimize repeated access.
+    Returns:
+        Settings: An instance of the Settings class with the application configuration.
+    """
     settings = Settings()
     print(f"Loading settings for: {settings.env_name}")
     return settings
