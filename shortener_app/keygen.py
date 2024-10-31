248b6a1199370eebd5e9f9a993e536aa975f3f54
Chris-Emma <chrisemmaofficial@gmail.com>
2024-10-31 08:20:09 +0000

    doctring documentation on keygen.py

--- shortener_app/keygen.py
+++ shortener_app/keygen.py
@@ -6,22 +6,30 @@ from . import crud
 
 def create_random_key(length: int = 5) -> str:
     """
-    Generate a random key with the specified length using uppercase letters and digits.
+    Generates a random key of the specified length using uppercase letters and digits.
+
     Parameters:
-    - length (int): Length of the random key (default is 5)
+    - length (int): Length of the random key (default is 5).
+
     Returns:
-    - str: Randomly generated key
+    - str: A randomly generated key consisting of uppercase letters and digits.
     """
     chars = string.ascii_uppercase + string.digits
     return "".join(secrets.choice(chars) for _ in range(length))
 
 def create_unique_random_key(db: Session) -> str:
     """
-    Generate a unique random key that is not already in use in the database.
+    Generates a unique random key not already in use in the database.
+
     Parameters:
-    - db (Session): SQLAlchemy database session
+    - db (Session): SQLAlchemy database session.
+
     Returns:
-    - str: Unique randomly generated key
+    - str: A unique, randomly generated key.
+    
+    Notes:
+    - Uses the `create_random_key` function to generate a key, then checks the database 
+      via the `crud.get_db_url_by_key` function to ensure the key does not already exist.
     """
     key = create_random_key()
     while crud.get_db_url_by_key(db, key):
